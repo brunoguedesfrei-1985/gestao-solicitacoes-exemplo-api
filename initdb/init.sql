6dd02e2367729f517b0e38bba7ceaaa9b3bdf433
Bruno Guedes Frei <bruno.guedes.frei@gmail.com>
2026-04-20 16:40:56 -0300

    sql

--- initdb/init.sql
+++ initdb/init.sql
@@ -35,3 +35,10 @@ CREATE TABLE IF NOT EXISTS solicitacao (
 INSERT INTO cargo (nome) VALUES ('gerente') ON CONFLICT DO NOTHING;
 INSERT INTO cargo (nome) VALUES ('analista') ON CONFLICT DO NOTHING;
 INSERT INTO cargo (nome) VALUES ('tecnico') ON CONFLICT DO NOTHING;
+
+INSERT INTO usuario
+(id, cpf, nome, email, senha, cargo_id)
+VALUES(1, '77689062768', 'sicrano alves', 'sicrano@gmail.com', '$2a$12$6R29ssoUy0DpUtVaDgIPDu94qyPldM/P9wUn7CWBFJTmKSEolv2OW', 2) ON CONFLICT DO NOTHING;
+INSERT INTO usuario
+(id, cpf, nome, email, senha, cargo_id)
+VALUES(2, '01182101062', 'fulano silva', 'fulano@gmail.com', '$2a$12$AqU9he2G4kUJvZ6VogddcevD2Yyu4AAiVTT4pNRKQzwtWTSr88sdy', 2) ON CONFLICT DO NOTHING;
